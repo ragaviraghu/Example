8c3f9bf05aefd615049b39519af3b3c0b45ddd97
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-02-21 08:48:14 -0800

    Create even_interval.cpp

---
+++ even_interval.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{int num1,num2;
+cout<<"Enter the first number : "<<endl;
+cin>>num1;
+cout<<"Enter the second number : "<<endl;
+cin>>num2;
+cout<<"The even numbers between the given two numbers is : "<<endl;
+for(int i=num1+1;i<num2;i++)
+{ if(i%2==0)
+{cout<<i<<endl;
+}
+}
+return 0;
+}
+
